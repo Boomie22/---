de2a5833dbcd86f8a9dd7ba8593be3c52212362d
Boomie22 <97672713+Boomie22@users.noreply.github.com>
2024-03-04 20:41:58 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <form className="login-form">
+        <h2>Login</h2>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
